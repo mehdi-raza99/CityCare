b826760937a415a7b2d42e5140c0cdf91f2e856e
Mehdi Raza <mehdics100@gmail.com>
2026-05-20 09:24:34 +0500

    fix: case sensitivity for ImpactStats

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import HeroSection from '../components/HeroSection/HeroSection'
 import Problems from '../components/CommonProblems/Problems'
-import ImpactStats from '../components/Impact-Stats/impactStats'
+import ImpactStats from '../components/Impact-Stats/ImpactStats'
 import CitizensTestimonials from '../components/Testimonials/CitizensTestimonials'
 import ContactUs from '../components/ContactUs/ContactUs'
 import Footer from '../components/Footer/Footer'
 export default function LandingPage() {
   return (
     <>
-     
+
       <HeroSection></HeroSection>
       <Problems></Problems>
       <ImpactStats></ImpactStats>
       <CitizensTestimonials></CitizensTestimonials>
       <ContactUs></ContactUs>
-     
+
     </>
   )
 }
